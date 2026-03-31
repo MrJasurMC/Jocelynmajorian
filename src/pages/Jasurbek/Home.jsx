bc26e0f8026fff15bd7e0921141671881c505c85
mrjasurmc <mrjasurmc@gmail.com>
2026-03-31 17:08:47 +0500

    men jasurbek hamma xatolarni to'g'irladm va homega o'zgartirish kiritdm

--- src/pages/Jasurbek/Home.jsx
+++ src/pages/Jasurbek/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import check from "./assets/check.png"
+import "./Home.css"
 
 function Home() {
   return (
@@ -13,49 +14,49 @@ function Home() {
         <h1>Nos domaines d’intervention :</h1>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Recrutement</h3>
           </div>
         </div>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Gestion des carrières, formation, fidélisation des équipes</h3>
           </div>
         </div>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Marque employeur, communication interne</h3>
           </div>
         </div>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Audit social, managérial et accompagnement opérationnel</h3>
           </div>
         </div>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Organisation temps de travail, plannings</h3>
           </div>
         </div>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Gestion administrative, application de la règlementation</h3>
           </div>
         </div>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Politique de rémunération</h3>
           </div>
         </div>
         <div className="nos-cards">
           <div className="nos-card">
-           <img src={check} alt="bu yerda check bor" />
+            <img src={check} alt="bu yerda check bor" />
             <h3>Relations sociales et gestion des conflits</h3>
           </div>
         </div>
